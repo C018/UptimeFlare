de8700eb78f4edf107f059fac439aa8eb3c5e51c
江楮 <43603829+C018@users.noreply.github.com>
2024-09-27 19:54:31 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -128,7 +128,7 @@ const workerConfig = {
       // responseKeyword: '',
       // [可选] 如果指定，检查将在你指定的区域运行，
       // 在设置此值之前，请参考文档 https://github.com/lyc8503/UptimeFlare/wiki/Geo-specific-checks-setup
-      // checkLocationWorkerRoute: ''
+      checkLocationWorkerRoute: 'https://twget.okr.pub/'
     },
     // 示例TCP监控
     {
