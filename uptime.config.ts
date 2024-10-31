66b84d7554c3f582f988fce71f5095fc712905f9
江楮 <43603829+C018@users.noreply.github.com>
2024-10-31 10:49:59 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -357,7 +357,7 @@ const workerConfig = {
     // 示例TCP监控
     {
       id: 'udpn-cn',
-      name: '中港专线国内段',
+      name: '中港专线国内三地段Server',
       // method对于tcp监控应该为TCP_PING
       method: 'TCP_PING',
       // target对于tcp监控应该为主机:端口
@@ -369,12 +369,25 @@ const workerConfig = {
     },
     // 示例TCP监控
     {
-      id: 'udpn-hk',
-      name: '中港专线香港段',
+      id: 'udpn-hkg',
+      name: '中港专线香港段Server-hkg',
       // method对于tcp监控应该为TCP_PING
       method: 'TCP_PING',
       // target对于tcp监控应该为主机:端口
-      target: '194.99.79.252:2053',
+      target: '185.244.208.40:2053',
+      tooltip: '中港专线香港段可用性监控',
+      // [可选] 如果指定，检查将在你指定的区域运行，
+      // 在设置此值之前，请参考文档 https://github.com/lyc8503/UptimeFlare/wiki/Geo-specific-checks-setup
+      checkLocationWorkerRoute: 'https://twget.okr.pub/'
+    },
+        // 示例TCP监控
+    {
+      id: 'udpn-hkg2',
+      name: '中港专线香港段Server-hkg2',
+      // method对于tcp监控应该为TCP_PING
+      method: 'TCP_PING',
+      // target对于tcp监控应该为主机:端口
+      target: '38.180.95.75:2053',
       tooltip: '中港专线香港段可用性监控',
       // [可选] 如果指定，检查将在你指定的区域运行，
       // 在设置此值之前，请参考文档 https://github.com/lyc8503/UptimeFlare/wiki/Geo-specific-checks-setup
